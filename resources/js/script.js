79a320b4ad94c63ecdb583263152782d76395ad7
shashank7200 <shashank7200@gmail.com>
2017-04-14 18:31:45 +0530

    Added logics for the keys and their key codes

--- resources/js/script.js
+++ resources/js/script.js
@@ -15,6 +15,12 @@ $(document).ready(function(){
 	var date = new Date().getFullYear();
 	document.getElementById('currentyear').innerHTML = date;
 	
+
+
+	/****** Key Coding **************/
+
+var keyCodes={3:"Break",8:"Backspace / Delete",9:"Tab",12:'Clear',13:"Enter",16:"Shift",17:"Ctrl",18:"Alt",19:"Pause/Break",20:"Caps lock",27:"Escape",32:"Spacebar",33:"Page up",34:"Page down",35:"End",36:"Home ",37:"Left arrow ",38:"Up arrow ",39:"Right arrow",40:"Down arrow ",41:"Select",42:"Print",43:"Execute",44:"Print Screen",45:"Insert ",46:"Delete",48:"0",49:"1",50:"2",51:"3",52:"4",53:"5",54:"6",55:"7",56:"8",57:"9",58:":",59:"Semicolon (firefox), equals",60:"<",61:"Equals (firefox)",63:"ß",64:"@ (firefox)",65:"A",66:"B",67:"C",68:"D",69:"E",70:"F",71:"G",72:"H",73:"I",74:"J",75:"K",76:"L",77:"M",78:"N",79:"O",80:"P",81:"Q",82:"R",83:"S",84:"T",85:"U",86:"V",87:"W",88:"X",89:"Y",90:"Z",91:"Windows Key / Left ⌘ / Chromebook Search key",92:"Right window key ",93:"Windows Menu / Right ⌘",96:"Numpad 0 ",97:"Numpad 1 ",98:"Numpad 2 ",99:"Numpad 3 ",100:"Numpad 4 ",101:"Numpad 5 ",102:"Numpad 6 ",103:"Numpad 7 ",104:"Numpad 8 ",105:"Numpad 9 ",106:"Multiply ",107:"Add",108:"Numpad period (firefox)",109:"Subtract ",110:"decimal point",111:"divide ",112:"f1 ",113:"f2 ",114:"f3 ",115:"f4 ",116:"f5 ",117:"f6 ",118:"f7 ",119:"f8 ",120:"f9 ",121:"f10",122:"f11",123:"f12",124:"f13",125:"f14",126:"f15",127:"f16",128:"f17",129:"f18",130:"f19",131:"f20",132:"f21",133:"f22",134:"f23",135:"f24",144:"num lock ",145:"scroll lock",160:"^",161:'!',163:"#",164:'$',165:'ù',166:"page backward",167:"page forward",169:"closing paren (AZERTY)",170:'*',171:"~ + * key",173:"minus (firefox), mute/unmute",174:"decrease volume level",175:"increase volume level",176:"next",177:"previous",178:"stop",179:"play/pause",180:"e-mail",181:"mute/unmute (firefox)",182:"decrease volume level (firefox)",183:"increase volume level (firefox)",186:"semi-colon / ñ",187:"equal sign ",188:"comma",189:"dash ",190:"period ",191:"forward slash / ç",192:"grave accent / ñ / æ",193:"?, / or °",194:"numpad period (chrome)",219:"open bracket ",220:"back slash ",221:"close bracket / å",222:"single quote / ø",223:"`",224:"left or right ⌘ key (firefox)",225:"altgr",226:"< /git >",230:"GNOME Compose Key",231:"ç",233:"XF86Forward",234:"XF86Back",255:"toggle touchpad"};
+
 	var keyCode = document.getElementById('key_codes');
 	var keyInfo = document.getElementById('key_info');
 	var welcomeInfo = document.getElementById('welcome_info');
@@ -27,8 +33,10 @@ $(document).ready(function(){
 		$(keyCode).show();
 		keyCode.textContent=event.keyCode || event.which;
 		$(keyInfo).show();
-		keyInfo.textContent=String.fromCharCode(event.keyCode || event.which);
+		keyInfo.textContent= keyCodes[event.keyCode || event.which];
+//			String.fromCharCode(event.keyCode || event.which);
 	});
+
 });
 
 // set global svg object
