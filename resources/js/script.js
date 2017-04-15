837f1080ed5970ff557402889155e2c15a76b4e2
Shashank Shekhar <shashank7200@gmail.com>
2017-04-15 15:34:57 +0530

    Added google analytics tag

--- resources/js/script.js
+++ resources/js/script.js
@@ -39,6 +39,18 @@ var keyCodes={3:"Break",8:"Backspace / Delete",9:"Tab",12:'Clear',13:"Enter",16:
 
 });
 
+// GOOGLE ANALYTICS TAG
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-97416980-1', 'auto');
+  ga('send', 'pageview');
+
+// END GOOGLE ANALYTICS TAG
+
 // set global svg object
 var svg = {};
 // used to determine which background to draw to
